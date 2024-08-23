a6ca2e3422c96df5f0f3a05f144bc53011517ef8
MPerezMenendez <tuteperezmenendez@gmail.com>
2024-08-23 11:00:15 -0300

    .

--- src/TP1/App.java
+++ src/TP1/App.java
@@ -1,52 +1,71 @@
-package com.template;
+package TP1;
 
 public class App {
 
     // 1. Sum of Two Integers
     public int sum(int a, int b) {
-        // TODO: Implement this method
-        return 0;
+        return a + b;
     }
 
     // 2. Check Even or Odd
     public boolean isEven(int number) {
-        // TODO: Implement this method
-        return false;
+        return (number % 2 == 0);
     }
 
     // 3. Maximum of Two Numbers
     public int max(int a, int b) {
-        // TODO: Implement this method
-        return 0;
+        if (a<b) {
+            return b;
+        }
+        return a;
     }
 
     // 4. Factorial of a Number
     public int factorial(int n) {
-        // TODO: Implement this method
-        return 0;
+        if (n==0){
+            return 1;
+        }
+        return n * factorial(n-1);
     }
 
     // 5. Count Characters in a String
     public int countChars(String input) {
-        // TODO: Implement this method
-        return 0;
+        return input.length();
     }
 
     // 6. Reverse a String
     public String reverse(String input) {
-        // TODO: Implement this method
+        StringBuilder string = new StringBuilder();
+        int start = input.length() -1;
+        for (int i = 0; i < input.length(); i++) {
+            string.append(input.charAt(start));
+            start--;
+        }
         return "";
     }
 
     // 7. Check Prime Number
     public boolean isPrime(int number) {
-        // TODO: Implement this method
-        return false;
+        if(number >= 1){
+            return false;
+        }
+        for (int i = number; i >= 0; i++){
+            if(number % i == 0){
+                return false;
+            }
+            return true;
+        }
     }
 
     // 8. Find the Smallest Element in an Array
     public int findMin(int[] array) {
-        // TODO: Implement this method
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if(array[i] < array[j]){
+                    return
+                }
+            }
+        }
         return 0;
     }
 
@@ -58,7 +77,6 @@ public class App {
 
     // 10. Convert Celsius to Fahrenheit
     public double celsiusToFahrenheit(double celsius) {
-        // TODO: Implement this method
-        return 0.0;
+        return celsius * ((double) 9 / 5) + 32;
     }
 }
--- src/Tests/AppTest.java
+++ src/Tests/AppTest.java
@@ -1,5 +1,6 @@
-package com.template;
+package Tests;
 
+import TP1.App;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
