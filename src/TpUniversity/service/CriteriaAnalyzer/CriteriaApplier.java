f529d697cbc2fc45775ad1528e0b6483cfb3e114
MPerezMenendez <tuteperezmenendez@gmail.com>
2024-11-05 21:17:25 -0300

    .

--- src/TpUniversity/service/CriteriaAnalyzer/CriteriaApplier.java
+++
@@ -1,8 +0,0 @@
-// File: src/TpUniversity/service/EvaluationCriteria.java
-package TpUniversity.service.CriteriaAnalyzer;
-
-import TpUniversity.model.Evaluations.Evaluation;
-
-public interface CriteriaApplier {
-    void apply(Evaluation evaluation, double value, String criteria);
-}
